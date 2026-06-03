f44e4442c0bfbb97d92fdd504b55f1c8b085d196
passk2233 <phetsamonesesomv@gmail.com>
2026-06-03 18:30:42 +0700

    ui fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,30 +42,3 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
-
-
-subprojects {
-    afterEvaluate {
-        val project = this
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            
-            // 1. บังคับโปรเจกต์ย่อยทุกตัว (ทั้ง Java และ Kotlin) ให้ใช้ Java 17
-            android.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-            
-            // 2. ดักจับปลั๊กอินย่อยที่ใช้ Kotlin แล้วเซฟค่า compiler ให้เป็น Java 17 แบบใหม่
-            project.plugins.withId("org.jetbrains.kotlin.android") {
-                val kotlinExtension = project.extensions.findByType(org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension::class.java)
-                kotlinExtension?.compilerOptions?.jvmTarget?.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-            }
-            
-            // 3. ดักจับเฉพาะงานคอมไพล์ Java ของทุกปลั๊กอินให้ใช้ Java 17 ตรงๆ
-            project.tasks.withType(JavaCompile::class.java).configureEach {
-                options.compilerArgs.addAll(listOf("-source", "17", "-target", "17"))
-            }
-        }
-    }
-}
